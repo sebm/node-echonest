97b6a437209c8daaaad49cb732f36d0f1c538457
Sebastian Motraghi <sebastian.m@gmail.com>
2011-08-18 00:08:34 -0500

    change test wording

--- test/echonest_api_v4_test.js
+++ test/echonest_api_v4_test.js
@@ -5,26 +5,26 @@ var vows = require('vows'),
 var theAPIKey = process.env.ECHONEST_API_KEY;
 
 vows.describe('EchoNest API v4 wrapper').addBatch({
-  'we can create an EchoNestAPI object': {
+  'An EchoNestAPI object': {
     topic: function () { return new EchoNestAPI(theAPIKey, { version:'4'}); },
 
-    'it is not totally borked': function (topic) {
+    'is successfully instantiated': function (topic) {
       assert.isNotNull(topic);
     },
-    'the howdy function works': function (topic) {
+    'has a "howdy" method': function (topic) {
       assert.equal(topic.howdy(), 'Howdy!');
     },
-    'the version number is set': function(topic) {
+    'has its version number set': function(topic) {
       assert.equal(topic.version, '4');
     },
-    'the apiKey is set': function(topic) {
+    'has its apiKey set': function(topic) {
       assert.equal(topic.apiKey, theAPIKey);
     },
-    'the execute method (when called correctly)' : {
+    'when its "execute method" is called correctly' : {
       topic: function (en) {
         en.execute('artist', 'hotttnesss', ['name'], { name : 'Ratatat'}, this.callback);
       },
-      'has some data': function (result) {
+      'results in data': function (result) {
         assert.isNotNull(result);
       }
     }
