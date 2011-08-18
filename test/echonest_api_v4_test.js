7bf056f3e7fb794aa3704db92cb031cd9ba32ede
Sebastian Motraghi <sebastian.m@gmail.com>
2011-08-17 19:20:13 -0500

    initialize test object with the appropriate apikey

--- test/echonest_api_v4_test.js
+++ test/echonest_api_v4_test.js
@@ -2,9 +2,11 @@ var vows = require('vows'),
   assert = require('assert'),
   EchoNestAPI = require('../index.js');
 
+var theAPIKey = process.env.ECHONEST_API_KEY;
+
 vows.describe('Basics!').addBatch({
   'we can create an EchoNestAPI object': {
-    topic: function () { return new EchoNestAPI('apikey', { version:'4'}); },
+    topic: function () { return new EchoNestAPI(theAPIKey, { version:'4'}); },
 
     'it is not totally borked': function (topic) {
       assert.isNotNull(topic);
