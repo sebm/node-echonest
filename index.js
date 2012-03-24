3850c4f949c2b6d6643802e25aab1741e9b9da0d
Syd Lawrence <sydlawrence@gmail.com>
2012-03-24 01:44:25 +0000

    Added sandbox access, including oauth for asset retreival

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-module.exports = require('./lib/echonest');
+
+EchoNestAPI = require('./lib/echonest');
+
+
+var apiKey = "{API_KEY}";
+
+var sandboxConfig = {
+	consumerKey: "{CONSUMER_KEY}",
+	consumerSecret: "{CONSUMER_SECRET}",
+	sandboxKey: "{SANDBOX_KEY}"
+};
+
+var echonest = EchoNestAPI(apiKey);
+
+var sandbox = echonest.sandbox(sandboxConfig);
+
+
+// Retrieve a list of assets
+sandbox.assets(function(data) {
+	console.log(data);
+});
+
+// Fetch an individual asset
+sandbox.access("{assetID}",function(data) {
+	console.log(data);
+})
+
+//  function assets($start = 0, $per_page=100)
